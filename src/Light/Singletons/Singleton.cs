1931d0c76690e6ad30181c7a98058fc658c40b17
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2024-10-22 21:52:33 +0300

    add singleton and steam integration

---
+++ src/Light/Singletons/Singleton.cs
@@ -0,0 +1,20 @@
+namespace Light;
+
+using Godot;
+
+/// <summary> Singletons perform a variety of tasks and, most often, are initialized immediately after starting the game in the root scene. </summary>
+public partial class Singleton<T> : Node
+{
+    static T instance;
+
+    /// <summary> Singleton instance available everywhere. </summary>
+    public static T Inst
+    {
+        get => instance;
+        set
+        {
+            instance = value;
+            GD.Print($"Initializing {typeof(T).Name} singleton...");
+        }
+    }
+}
---
+++ src/Light/Singletons/Steam.cs
@@ -0,0 +1,19 @@
+namespace Light;
+
+using Godot;
+using Steamworks;
+
+/// <summary> Singleton responsible for the authentication in Steam and backend updating. </summary>
+public partial class Steam : Singleton<Steam>
+{
+	/// <summary> Application identifier in Steam. </summary>
+	[Export] public uint AppID;
+
+	public override void _EnterTree() => SteamClient.Init(AppID, false);
+
+	public override void _ExitTree() => SteamClient.Shutdown();
+
+	public override void _Ready() => Inst = this;
+
+	public override void _PhysicsProcess(double delta) => SteamClient.RunCallbacks();
+}
