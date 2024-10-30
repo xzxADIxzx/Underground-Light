2c7437fbc7aff1c2aa9d83054c3ba21c12ff0bbb
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2024-10-30 15:09:44 +0300

    AAAAAA SPACES

--- src/Light/Singletons/Steam.cs
+++ src/Light/Singletons/Steam.cs
@@ -6,14 +6,14 @@ using Steamworks;
 /// <summary> Singleton responsible for the authentication in Steam and backend updating. </summary>
 public partial class Steam : Singleton<Steam>
 {
-	/// <summary> Application identifier in Steam. </summary>
-	[Export] public uint AppID;
+    /// <summary> Application identifier in Steam. </summary>
+    [Export] public uint AppID;
 
-	public override void _EnterTree() => SteamClient.Init(AppID, false);
+    public override void _EnterTree() => SteamClient.Init(AppID, false);
 
-	public override void _ExitTree() => SteamClient.Shutdown();
+    public override void _ExitTree() => SteamClient.Shutdown();
 
-	public override void _Ready() => Inst = this;
+    public override void _Ready() => Inst = this;
 
-	public override void _PhysicsProcess(double delta) => SteamClient.RunCallbacks();
+    public override void _PhysicsProcess(double delta) => SteamClient.RunCallbacks();
 }
