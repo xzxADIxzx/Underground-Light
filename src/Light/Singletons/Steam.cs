f6df40962b3fd401be9623323c34f9ca03792774
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2024-11-05 20:52:46 +0300

    add account id caching to steam

--- src/Light/Singletons/Steam.cs
+++ src/Light/Singletons/Steam.cs
@@ -2,14 +2,31 @@ namespace Light.Singletons;
 
 using Godot;
 using Steamworks;
+using System;
 
 /// <summary> Singleton responsible for the authentication in Steam and backend updating. </summary>
 public partial class Steam : Singleton<Steam>
 {
+    /// <summary> User account identifier in Steam. </summary>
+    public static uint AccId;
     /// <summary> Application identifier in Steam. </summary>
-    [Export] public uint AppID;
+    [Export] public uint AppId;
 
-    public override void _EnterTree() => SteamClient.Init(AppID, false);
+    public override void _EnterTree()
+    {
+        try
+        {
+            // connect to the local client of Steam
+            SteamClient.Init(AppId, false);
+
+            // cache the id, because SteamAPI_ISteamUser_GetSteamID is incredibly slow
+            AccId = SteamClient.SteamId.AccountId;
+        }
+        catch (Exception ex)
+        {
+            Log.Error($"Couldn't initialize Steam: {ex}");
+        }
+    }
 
     public override void _ExitTree() => SteamClient.Shutdown();
 
